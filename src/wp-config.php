d03364108b808b5c0e2b6a10f1a9a949c01447e9
danny <dannywang@augmentum.com.cn>
2016-12-22 15:24:01 +0800

    feat(dev-iwish): add dev config

--- src/wp-config.php
+++ src/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'bdm251568552_db');
+define('DB_NAME', 'iwish');
 
 /** MySQL数据库用户名 */
-define('DB_USER', 'bdm251568552');
+define('DB_USER', 'root');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', '7563836ab');
+define('DB_PASSWORD', '123456');
 
 /** MySQL主机 */
-define('DB_HOST', 'bdm251568552.my3w.com');
+define('DB_HOST', '127.0.0.1');
 
 /** 创建数据表时默认的文字编码 */
 define('DB_CHARSET', 'utf8');
